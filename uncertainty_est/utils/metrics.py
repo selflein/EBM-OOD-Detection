8011bdb85602c12fbd89837a7ffed1267e869931
Sven Elflein <elflein.sven@googlemail.com>
2020-12-06 13:10:31 +0100

    Add Brier decomposition metric

--- uncertainty_est/utils/metrics.py
+++ uncertainty_est/utils/metrics.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+from sklearn.metrics import confusion_matrix
 
 
 def accuracy(y: torch.Tensor, y_hat: torch.Tensor):
@@ -13,7 +14,46 @@ def brier_score(labels, probs):
     return score
 
 
-def classification_calibration(labels, probs, bins=10, tag=""):
+def brier_decomposition(labels, probs):
+    """Compute the decompositon of the Brier score into its three components
+     uncertainty, reliability and resolution. Brier score is given by
+     `BS = REL - RES + UNC`. Discretization into probability classes `M_k` is done for
+     `p_k > p_i` for all `i!=k`. This induces a error when compared to the Brier score.
+
+     Args:
+        labels: Numpy array of shape (num_preds,) containing the groundtruth
+         class in range [0, n_classes - 1].
+        probs: Numpy array of shape (num_preds, n_classes) containing predicted
+         probabilities for the classes.
+
+    Returns:
+        (uncertainty, resolution, relability): Additive components of the Brier
+         score decomposition.
+    """
+    preds = np.argmax(probs, dim=1)
+    conf_mat = confusion_matrix(labels, preds, labels=np.arange(probs.shape[1]))
+
+    pbar = np.sum(conf_mat, axis=0)
+    pbar /= pbar.sum()
+
+    dist_weights = np.sum(conf_mat, axis=1)
+    dist_weights /= dist_weights.sum()
+
+    dist_mean = conf_mat / (np.sum(conf_mat, axis=1)[:, None] + 1e-7)
+
+    uncertainty = -np.sum(pbar ** 2)
+
+    resolution = (pbar[:, None] - dist_mean) ** 2
+    resolution = np.sum(dist_weights * np.sum(resolution, axis=1))
+
+    prob_true = np.take(dist_mean, preds, axis=0)
+    reliability = np.sum((prob_true - probs) ** 2, axis=1)
+    reliability = np.mean(reliability)
+
+    return uncertainty, resolution, reliability
+
+
+def classification_calibration(labels, probs, bins=10):
     preds = np.argmax(probs, axis=1)
     total = labels.shape[0]
     probs = np.max(probs, axis=1)
