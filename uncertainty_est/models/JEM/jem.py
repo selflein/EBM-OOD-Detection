MCMC hyperparameter search, fix log-likelihood approximaion in EBMs

uncertainty_est/models/JEM/jem.py
@@ -7,9 +7,9 @@ import pytorch_lightning as pl
 import torch.nn.functional as EBM
 from pytorch_lightning.core.decorators import auto_move_data
 
-from uncertainty_est.utils.utils import to_np
 from uncertainty_est.archs.arch_factory import get_arch
 from uncertainty_est.models.JEM.model import EBM, ConditionalEBM
+from uncertainty_est.utils.utils import to_np, estimate_normalizing_constant
 from uncertainty_est.models.JEM.utils import (
     KHotCrossEntropyLoss,
     smooth_one_hot,
@@ -158,16 +158,18 @@ class JEM(pl.LightningModule):
         self.log("test_acc", acc)
 
     def test_epoch_end(self, logits):
+        if not self.is_toy_dataset:
+            return
+
         # Estimate normalizing constant Z by numerical integration
-        interp = torch.linspace(-10, 10, 1000)
-        interval = 20.0 / 1000.0
-        x, y = torch.meshgrid(interp, interp)
-        grid = torch.stack((x.reshape(-1), y.reshape(-1)), 1)
-        log_ex = self.model(grid.to(self.device))
-        log_Z = torch.sum(log_ex * (interval ** 2))
+        log_Z = torch.log(
+            estimate_normalizing_constant(
+                lambda x: -self(x).logsumexp(1), device=self.device
+            )
+        )
 
         log_px = logits.logsumexp(1) - log_Z
-        self.log("test_log_likelihood", log_px.mean())
+        self.log("log_likelihood", log_px.mean())
 
     def configure_optimizers(self):
         optim = torch.optim.AdamW(

uncertainty_est/models/JEM/vera.py
@@ -7,9 +7,9 @@ import torch.nn.functional as tf
 from torch import distributions
 import matplotlib.pyplot as plt
 
-from uncertainty_est.utils.utils import to_np
 from uncertainty_est.archs.arch_factory import get_arch
 from uncertainty_est.models.JEM.model import EBM, ConditionalEBM
+from uncertainty_est.utils.utils import to_np, estimate_normalizing_constant
 from uncertainty_est.models.JEM.vera_utils import (
     VERAGenerator,
     VERAHMCGenerator,
@@ -213,12 +213,11 @@ class VERA(pl.LightningModule):
             return
 
         # Estimate normalizing constant Z by numerical integration
-        interp = torch.linspace(-10, 10, 1000)
-        interval = 20.0 / 1000.0
-        x, y = torch.meshgrid(interp, interp)
-        grid = torch.stack((x.reshape(-1), y.reshape(-1)), 1)
-        log_ex = self.model(grid.to(self.device))
-        log_Z = torch.sum(log_ex * (interval ** 2))
+        log_Z = torch.log(
+            estimate_normalizing_constant(
+                lambda x: self(x).exp().sum(1), device=self.device
+            )
+        )
 
         logits = torch.cat(logits, 0)
         log_px = logits.logsumexp(1) - log_Z

uncertainty_est/models/normalizing_flow/norm_flow.py
@@ -66,7 +66,7 @@ class NormalizingFlow(pl.LightningModule):
     def test_step(self, batch, batch_idx):
         x, _ = batch
         log_p = self.density_estimation.log_prob(x)
-        self.log("test_log_likelihood", log_p.mean())
+        self.log("log_likelihood", log_p.mean())
 
     def configure_optimizers(self):
         optim = torch.optim.AdamW(

uncertainty_est/train.py
@@ -1,5 +1,9 @@
+# Fix https://github.com/pytorch/pytorch/issues/37377
+import numpy as _
+
 import os
 import sys
+from uuid import uuid4
 
 sys.path.insert(0, os.getcwd())
 
@@ -48,6 +52,8 @@ def run(
     data_shape,
     sigma=0.0,
     output_folder=None,
+    log_dir="logs",
+    num_workers=4,
 ):
     torch.set_default_tensor_type(torch.FloatTensor)
     pl.seed_everything(seed)
@@ -61,6 +67,7 @@ def run(
         data_shape=data_shape,
         ood_dataset=ood_dataset,
         sigma=sigma,
+        num_workers=num_workers,
     )
     val_loader = get_dataloader(
         dataset,
@@ -69,6 +76,7 @@ def run(
         data_shape=data_shape,
         sigma=sigma,
         ood_dataset=ood_dataset,
+        num_workers=num_workers,
     )
     test_loader = get_dataloader(
         dataset,
@@ -77,6 +85,7 @@ def run(
         data_shape=data_shape,
         sigma=sigma,
         ood_dataset=ood_dataset,
+        num_workers=num_workers,
     )
 
     output_folder = (
@@ -84,7 +93,7 @@ def run(
         if output_folder is None
         else output_folder
     )
-    out_path = Path("logs") / model_name / dataset / output_folder
+    out_path = Path(log_dir) / model_name / dataset / f"{output_folder}_{uuid4()}"
     out_path.mkdir(exist_ok=False, parents=True)
 
     callbacks = []