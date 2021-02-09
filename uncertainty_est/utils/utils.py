7c5d2f3562a6eafe45f38bef4ce1c04b4b999a10
Sven Elflein <elflein.sven@googlemail.com>
2021-02-09 15:04:09 +0100

    Add numerical integration for higher dims

--- uncertainty_est/utils/utils.py
+++ uncertainty_est/utils/utils.py
@@ -8,17 +8,41 @@ def to_np(tensor: torch.Tensor):
 
 
 def estimate_normalizing_constant(
-    density_func, interval=(-10, 10), samples=1000, device="cpu"
+    density_func,
+    interval=(-10, 10),
+    num_samples=1000,
+    device="cpu",
+    dimensions=2,
+    batch_size=1024,
 ):
     """
-    Numericall integrate a EBM in the specified interval. Only works for 2D datasets.
+    Numerically integrate a funtion in the specified interval.
     """
-    interp = torch.linspace(*interval, samples)
-    x, y = torch.meshgrid(interp, interp)
-    grid = torch.stack((x.reshape(-1), y.reshape(-1)), 1)
-    p_x = density_func(grid.to(device))
+    interp = torch.linspace(*interval, num_samples)
+    grid_coords = torch.meshgrid(*[interp for _ in range(dimensions)])
+    grid = torch.stack([coords.reshape(-1) for coords in grid_coords], 1)
 
-    dx = (abs(interval[0]) + abs(interval[1])) / samples
-    Z = trapz(trapz(to_np(p_x).reshape(len(interp), len(interp)), dx=dx), dx=dx)
+    p_x = []
+    for samples in torch.split(grid, batch_size):
+        p_x.append(density_func(samples.to(device)))
+    p_x = torch.cat(p_x)
 
-    return torch.tensor(Z)
+    dx = (abs(interval[0]) + abs(interval[1])) / num_samples
+    # Integrate one dimension after another
+    grid_vals = to_np(p_x).reshape(*[len(interp) for _ in range(dimensions)])
+    for _ in range(dimensions):
+        grid_vals = trapz(grid_vals, dx=dx, axis=-1)
+
+    return torch.tensor(grid_vals)
+
+
+if __name__ == "__main__":
+    dims = 2
+    samples = 100
+    print(
+        estimate_normalizing_constant(
+            lambda x: torch.empty(x.shape[0]).fill_(1 / (samples ** dims)),
+            num_samples=samples,
+            dimensions=dims,
+        )
+    )
