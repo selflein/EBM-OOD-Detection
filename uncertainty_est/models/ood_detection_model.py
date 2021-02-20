bb6119184fd7a7cc5203c50e519fe44adb787c83
Sven Elflein <elflein.sven@googlemail.com>
2021-02-20 11:39:07 +0100

    Monkey patch  to prevent deepcopy error

--- uncertainty_est/models/ood_detection_model.py
+++ uncertainty_est/models/ood_detection_model.py
@@ -1,12 +1,15 @@
 import abc
+import inspect
 from os import path
 from typing import Dict
+import copy
 
 import torch
 import numpy as np
 import pytorch_lightning as pl
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_auc_score, average_precision_score
+from pytorch_lightning.utilities.parsing import get_init_args
 
 from uncertainty_eval.vis import plot_score_hist
 
@@ -34,7 +37,7 @@ class OODDetectionModel(pl.LightningModule):
             for score_name, id_scores in id_scores_dict.items():
                 ood_scores = ood_scores_dict[score_name]
 
-                if self.logger.save_dir is not None:
+                if self.logger is not None and self.logger.save_dir is not None:
                     ax = plot_score_hist(
                         id_scores,
                         ood_scores,
@@ -82,3 +85,40 @@ class OODDetectionModel(pl.LightningModule):
                 pg["lr"] = lr_scale * self.hparams.learning_rate
 
         optimizer.step(closure=optimizer_closure)
+
+    # TODO: Remove when https://github.com/PyTorchLightning/pytorch-lightning/pull/6056 is merged
+    def save_hyperparameters(
+        self, *args, ignore=["test_ood_dataloaders"], frame=None
+    ) -> None:
+        if not frame:
+            frame = inspect.currentframe().f_back
+        init_args = get_init_args(frame)
+        assert init_args, "failed to inspect the self init"
+
+        if ignore is not None:
+            if isinstance(ignore, str):
+                ignore = [ignore]
+            if isinstance(ignore, (list, tuple)):
+                ignore = [arg for arg in ignore if isinstance(arg, str)]
+            init_args = {k: v for k, v in init_args.items() if k not in ignore}
+
+        if not args:
+            # take all arguments
+            hp = init_args
+            self._hparams_name = "kwargs" if hp else None
+        else:
+            # take only listed arguments in `save_hparams`
+            isx_non_str = [i for i, arg in enumerate(args) if not isinstance(arg, str)]
+            if len(isx_non_str) == 1:
+                hp = args[isx_non_str[0]]
+                cand_names = [k for k, v in init_args.items() if v == hp]
+                self._hparams_name = cand_names[0] if cand_names else None
+            else:
+                hp = {arg: init_args[arg] for arg in args if isinstance(arg, str)}
+                self._hparams_name = "kwargs"
+
+        # `hparams` are expected here
+        if hp:
+            self._set_hparams(hp)
+        # make deep copy so  there is not other runtime changes reflected
+        self._hparams_initial = copy.deepcopy(self._hparams)
