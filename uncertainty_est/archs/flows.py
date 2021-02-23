Fix determinant computation in Approximate NF

uncertainty_est/archs/flows.py
@@ -35,8 +35,9 @@ class OrthogonalTransform(nn.Module):
 
 
 class ReparameterizedTransform(nn.Module):
-    def __init__(self, dim):
+    def __init__(self, dim, nonlin=True):
         super().__init__()
+        self.nonlin = nonlin
         self.dim = dim
         self.bias = nn.Parameter(torch.zeros(dim))
 
@@ -48,10 +49,14 @@ class ReparameterizedTransform(nn.Module):
 
     def forward(self, x):
         weight = self.u_mat @ torch.diag_embed(self.sigma) @ self.v_mat.T
-        return torch.nn.functional.linear(x, weight, self.bias)
+        lin_out = torch.nn.functional.linear(x, weight, self.bias)
+        return lin_out if not self.nonlin else torch.exp(lin_out)
 
     def log_abs_det_jacobian(self, z, z_next):
-        return torch.sum(self.sigma)
+        if self.nonlin:
+            return torch.sum(self.sigma).abs()
+        else:
+            return torch.log(torch.prod(self.sigma).abs())
 
     def compute_weight_penalty(self):
         return self._weight_penalty(self.u_mat) + self._weight_penalty(self.v_mat)
@@ -91,7 +96,10 @@ class NormalizingFlowDensity(nn.Module):
             )
         elif self.flow_type == "reparameterized_flow":
             self.transforms = nn.ModuleList(
-                [ReparameterizedTransform(dim) for _ in range(flow_length)]
+                [
+                    ReparameterizedTransform(dim, nonlin=(i != (flow_length - 1)))
+                    for i in range(flow_length)
+                ]
             )
         else:
             raise NotImplementedError