d2aec648cd301f4a76f17d2ba6f0a350dd4fac70
Sven Elflein <elflein.sven@googlemail.com>
2021-03-01 15:19:35 +0100

    Minor fix for RealNVP

--- uncertainty_est/archs/real_nvp/real_nvp.py
+++ uncertainty_est/archs/real_nvp/real_nvp.py
@@ -110,7 +110,7 @@ class RealNVP(nn.Module):
     def log_prob(self, x):
         z, sldj = self.forward(x, reverse=False)
         prior_ll = -0.5 * (z ** 2 + np.log(2 * np.pi))
-        prior_ll = prior_ll.view(z.size(0), -1).sum(-1) - np.log(self.k) * np.prod(
+        prior_ll = prior_ll.reshape(z.size(0), -1).sum(-1) - np.log(self.k) * np.prod(
             z.size()[1:]
         )
         ll = prior_ll + sldj
