38b85afe09d2772534b715069321782a95f0ab1a
Sven Elflein <elflein.sven@googlemail.com>
2021-03-25 17:08:29 +0100

    Add NCE training

---
+++ uncertainty_est/models/EBM/nce.py
@@ -0,0 +1,140 @@
+from collections import defaultdict
+
+import torch
+from tqdm import tqdm
+import pytorch_lightning as pl
+import torch.nn.functional as F
+from torch import distributions
+import matplotlib.pyplot as plt
+
+from uncertainty_est.archs.arch_factory import get_arch
+from uncertainty_est.models.ood_detection_model import OODDetectionModel
+from uncertainty_est.utils.utils import to_np, estimate_normalizing_constant
+
+
+class NoiseContrastiveEstimation(OODDetectionModel):
+    """Implementation of Noise Contrastive Estimation http://proceedings.mlr.press/v9/gutmann10a.html"""
+
+    def __init__(
+        self,
+        arch_name,
+        arch_config,
+        learning_rate,
+        momentum,
+        weight_decay,
+        noise_distribution="uniform",
+        noise_distribution_kwargs={"low": 0, "high": 1},
+        is_toy_dataset=False,
+        toy_dataset_dim=2,
+        test_ood_dataloaders=[],
+    ):
+        super().__init__()
+        self.__dict__.update(locals())
+        self.save_hyperparameters()
+
+        self.model = get_arch(arch_name, arch_config)
+
+        if noise_distribution == "uniform":
+            noise_dist = distributions.Uniform
+        else:
+            raise NotImplementedError(
+                f"Requested noise distribution {noise_distribution} not implemented."
+            )
+        self.noise_dist = noise_dist(**noise_distribution_kwargs)
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        x, _ = batch
+
+        noise = self.noise_dist.sample(x.shape)
+        inp = torch.cat((x, noise))
+
+        log_p_model = self.model(inp).squeeze()
+        log_p_noise = self.noise_dist.log_prob(inp)
+
+        loss = F.binary_cross_entropy_with_logits(
+            log_p_model - log_p_noise,
+            torch.cat(torch.ones(len(x)), torch.zeros(len(x))),
+        )
+
+        self.log("train/loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        return
+
+    def validation_epoch_end(self, outputs):
+        if self.is_toy_dataset and self.toy_dataset_dim == 2:
+            interp = torch.linspace(-4, 4, 500)
+            x, y = torch.meshgrid(interp, interp)
+            data = torch.stack((x.reshape(-1), y.reshape(-1)), 1)
+            p_xy = torch.exp(self(data.to(self.device)))
+            px = to_np(p_xy.sum(1))
+
+            x, y = to_np(x), to_np(y)
+            for i in range(p_xy.shape[1]):
+                fig, ax = plt.subplots()
+                mesh = ax.pcolormesh(x, y, to_np(p_xy[:, i]).reshape(*x.shape))
+                fig.colorbar(mesh)
+                self.logger.experiment.add_figure(
+                    f"dist/p(x,y={i})", fig, self.current_epoch
+                )
+                plt.close()
+
+            fig, ax = plt.subplots()
+            mesh = ax.pcolormesh(x, y, px.reshape(*x.shape))
+            fig.colorbar(mesh)
+            self.logger.experiment.add_figure("dist/p(x)", fig, self.current_epoch)
+            plt.close()
+
+    def test_step(self, batch, batch_idx):
+        self.to(torch.float32)
+        x, y = batch
+        y_hat = self.model(x)
+
+        acc = (y == y_hat.argmax(1)).float().mean(0).item()
+        self.log("test_acc", acc)
+        return y_hat
+
+    def test_epoch_end(self, logits):
+        if self.is_toy_dataset:
+            # Estimate normalizing constant Z by numerical integration
+            log_Z = torch.log(
+                estimate_normalizing_constant(
+                    lambda x: self(x).exp().sum(1),
+                    device=self.device,
+                    dimensions=self.toy_dataset_dim,
+                    dtype=torch.float32,
+                )
+            )
+
+            logits = torch.cat(logits, 0)
+            log_px = logits.logsumexp(1) - log_Z
+            self.log("log_likelihood", log_px.mean())
+
+        super().test_epoch_end()
+
+    def configure_optimizers(self):
+        optim = torch.optim.AdamW(
+            self.parameters(),
+            betas=(self.momentum, 0.999),
+            lr=self.learning_rate,
+            weight_decay=self.weight_decay,
+        )
+        scheduler = torch.optim.lr_scheduler.StepLR(optim, step_size=30, gamma=0.5)
+        return [optim], [scheduler]
+
+    def ood_detect(self, loader):
+        self.eval()
+        torch.set_grad_enabled(False)
+        scores = []
+        for x, y in tqdm(loader):
+            x = x.to(self.device)
+            score = self.model(x).cpu()
+            scores.append(score)
+
+        uncert = {}
+        uncert["p(x)"] = torch.cat(scores).cpu().numpy()
+        return uncert
--- uncertainty_est/models/__init__.py
+++ uncertainty_est/models/__init__.py
@@ -1,37 +1,21 @@
-from uncertainty_est.models.JEM.hdge import HDGEModel
-from uncertainty_est.models.JEM.jem import JEM
-from uncertainty_est.models.JEM.jem_priornet import JEMPriorNet
-from uncertainty_est.models.JEM.hdge_priornet import HDGEPriorNetModel
-from uncertainty_est.models.ce_baseline import CEBaseline
-from uncertainty_est.models.energy_finetuning import EnergyFinetune
-from uncertainty_est.models.priornet.priornet import PriorNet
-from uncertainty_est.models.JEM.vera import VERA
-from uncertainty_est.models.JEM.vera_priornet import VERAPriorNet
-from uncertainty_est.models.JEM.vera_posteriornet import VERAPosteriorNet
-from uncertainty_est.models.normalizing_flow.norm_flow import NormalizingFlow
-from uncertainty_est.models.normalizing_flow.approx_flow import ApproxNormalizingFlow
-from uncertainty_est.models.EBM.regularized_ebm import RegularizedEBM
-from uncertainty_est.models.EBM.per_sample_nce import PerSampleNCE
-from uncertainty_est.models.normalizing_flow.iresnet import IResNetFlow
-from .normalizing_flow.real_nvp import RealNVPModel
-from .vae import VAE
+from uncertainty_est import models
 
 MODELS = {
-    "HDGE": HDGEModel,
-    "JEM": JEM,
-    "CEBaseline": CEBaseline,
-    "EnergyOOD": EnergyFinetune,
-    "PriorNet": PriorNet,
-    "JEMPriorNet": JEMPriorNet,
-    "HDGEPriorNet": HDGEPriorNetModel,
-    "VERA": VERA,
-    "VERAPriorNet": VERAPriorNet,
-    "VERAPosteriorNet": VERAPosteriorNet,
-    "NormalizingFlow": NormalizingFlow,
-    "ApproxNormalizingFlow": ApproxNormalizingFlow,
-    "RegularizedEBM": RegularizedEBM,
-    "PerSampleNCE": PerSampleNCE,
-    "IResNet": IResNetFlow,
-    "RealNVP": RealNVPModel,
-    "VAE": VAE,
+    "HDGE": models.JEM.hdge.HDGEModel,
+    "JEM": models.JEM.jem.JEM,
+    "CEBaseline": models.ce_baseline.CEBaseline,
+    "EnergyOOD": models.energy_finetuning.EnergyFinetune,
+    "PriorNet": models.priornet.priornet.PriorNet,
+    "JEMPriorNet": models.JEM.jem_priornet.JEMPriorNet,
+    "VERA": models.JEM.vera.VERA,
+    "VERAPriorNet": models.JEM.vera_priornet.VERAPriorNet,
+    "VERAPosteriorNet": models.JEM.vera_posteriornet.VERAPosteriorNet,
+    "NormalizingFlow": models.normalizing_flow.norm_flow.NormalizingFlow,
+    "ApproxNormalizingFlow": models.normalizing_flow.approx_flow.ApproxNormalizingFlow,
+    "RegularizedEBM": models.EBM.regularized_ebm.RegularizedEBM,
+    "PerSampleNCE": models.EBM.per_sample_nce.PerSampleNCE,
+    "IResNet": models.normalizing_flow.iresnet.IResNetFlow,
+    "RealNVP": models.normalizing_flow.real_nvp.RealNVPModel,
+    "VAE": models.vae.VAE,
+    "NCE": models.EBM.nce.NoiseContrastiveEstimation,
 }
